8a47d06aefd871156a7e4ae57943ff54f8920647
Tim Handy <tjhandy@gmail.com>
2016-09-06 22:36:20 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+/*
+
+Create
+Render
+Update
+Delete
+
+recipe object - JSON?
+    title
+    createdDate
+    updatedDate
+    type: breakfast, main, snack, dessert
+    serves: integer
+    proteinSource: red meat flag, veg, lentils etc?
+    category: carnivore, vegan, vegetarian
+    prepTime: integer, mins
+    cookTime: integer, mins
+    ingredients: obj, ingredient, qtyType: ml/g/oz/handful, qty: int
+    method: string, or stepNum: instructions?
+    comments: string
+    favourite: true/false
+	calories: integer
+	photo: image
+	author: string
+
+
+User story: As an editor I can CRUD meals. Delete should hide, not delete?
+User story: As a user I can generate a list of x semi-random meals for the week (only 1 red meat for example)
+User story: As an editor I can CRUD meals. Delete should hide, not delete?
+User story: As a user I can click on a category to filter by type of meal so that I can scroll through/or otherwise look at interesting recipes
+User story: As a user I can view an image of the recipes
+
+# IDEA: option to export all recipes to csv
+# TODO: add some bootstrap formatting
+# TODO: font-family: "Essays 1743", Palatino, "Palatino Linotype", "Book Antiqua", Georgia, "Times New Roman", serif;
+# TODO: Display mealplan for the week - select x random main course recipes, include x fish and x red meat etc, x desserts, must contain a Tim's favorite.
+# TODO: Display out the ingredients for the 6 meals, e.g. creating a shopping list
+# IDEA: Display mealplan without garlic
+# IDEA: Display a random dinner / pick a random favourite
+# IDEA: Display recipes containing the word/s x in name
+# IDEA: If weekend allow not healthy in selecting/generating meals
+# IDEA: Display vege/vegan meals
+# IDEA: Display recipes containing >= 1 of a list of ingredients entered, in order of most ingredients found
+# TODO: Add a search function. Think bootstrap has this? Not sure about the engine though.
+# FIXME: Prevent a blank recipe being added.
+# IDEA: enable logins for friends/family
+# IDEA: notifications of who updated/added what. Could just be a header on the front page.
+
+*/
